7f83ca67072c760b58d7b732906a7d9f99ba3eea
austinmscholl <asprof34@gmail.com>
2018-12-11 01:21:19 -0500

    getting back into it

---
+++ Grades/Grades.Tests/Types/ReferenceTypeTests.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades.Tests.Types
+{
+    [TestClass]
+    public class TypeTests
+    {
+        [TestMethod]
+        public void ValueTypesPassByValue()
+        {
+            int x = 46;
+            IncrementNumber(x);
+
+            Assert.AreEqual(x, 46);
+        }
+
+        private void IncrementNumber(int number)
+        {
+            number += 1;
+        }
+
+        [TestMethod]
+        public void ReferenceTypesPassByValue()
+        {
+            GradeBook book1 = new GradeBook();
+            GradeBook book2 = book1;
+
+            GiveBookAName(book2);
+
+            Assert.AreEqual("A GradeBook", book1.Name);
+        }
+
+        private void GiveBookAName(GradeBook book)
+        {
+            book.Name = "A GradeBook";
+        }
+
+        [TestMethod]
+        public void StringComparisons()
+        {
+            string s1 = "Scott";
+            string s2 = "scott";
+
+            bool result = String.Equals(s1, s2, StringComparison.InvariantCultureIgnoreCase);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IntVariablesHoldAValue()
+        {
+            int x = 10;
+            int y = x;
+
+            x = 4;
+
+            Assert.AreNotEqual(x, y);
+        }
+
+        [TestMethod]
+        public void GradebookVariablesHoldReference()
+        {
+            GradeBook g1 = new GradeBook();
+            GradeBook g2 = g1;
+
+            g1.Name = "Scott's gradebook";
+            Assert.AreEqual(g1.Name, g2.Name);
+        }
+    }
+}
